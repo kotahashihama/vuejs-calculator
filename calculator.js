cde988d9001de1e06d375cd1e725d498115d4afb
Kota Hashihama <kotahashihama@gmail.com>
2019-04-26 12:57:32 +0900

    Add calculating function

--- calculator.js
+++ calculator.js
@@ -2,5 +2,17 @@ var cal = new Vue({
   el: '#cal',
   data: {
     value: 0
+  },
+  methods: {
+    addValue: function (value) {
+      if (this.value == 0 && typeof value != 'string') {
+        this.value = ''
+      }
+      this.value += value + ''
+    },
+    execute: function () {
+      this.value = eval(this.value)
+
+    }
   }
 })
